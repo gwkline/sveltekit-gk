bae24981e0c025978f17746cfa47b154c9d6c33b
Gavin Kline <gwkline23@gmail.com>
2023-10-08 17:42:46 -0400

    animating view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -221,7 +221,7 @@
 		flex-direction: column;
 	}
 
-	/* @keyframes fade-in {
+	@keyframes fade-in {
 		from {
 			opacity: 0;
 		}
@@ -255,5 +255,5 @@
 		animation:
 			210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
 			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
-	} */
+	}
 </style>
